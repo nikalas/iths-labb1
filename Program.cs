37cf1c68a1a1002118769bbb7546492256b9f880
Niklas Eliason <9056489+nikalas@users.noreply.github.com>
2026-09-17 21:23:43 +0200

    add iterator implementation

--- Program.cs
+++ Program.cs
@@ -163,3 +163,28 @@ class NumberSequenceFinderClass
 	}
 
 }
+
+class NumberSequenceFinderIterator
+{
+	public static IEnumerable<(string NumberSequence, int StartIndex, int EndIndex)> Find(string input)
+	{
+		for (int i = 0; i < input.Length; i++)
+		{
+			char startDigit = input[i];
+			for (int j = i + 1; j < input.Length; j++)
+			{
+				if (!char.IsDigit(input[j]))
+				{
+					break;
+				}
+
+				if (input[j] == startDigit)
+				{
+					string numberSequence = input.Substring(i, j - i + 1);
+					yield return (numberSequence, i, j);
+					break;
+				}
+			}
+		}
+	}
+}
